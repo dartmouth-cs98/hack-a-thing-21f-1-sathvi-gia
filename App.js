91938f71f034dc8653f5f93c4e9985f061a040c7
Sathvi Korandla <sathvika.korandla@gmail.com>
2021-09-18 17:49:47 -0400

    add buttons

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+
 import {
 	NoteOne,
 	NoteTwo,
@@ -7,3 +9,77 @@ import {
 	NoteSix,
 	NoteSeven
 } from './constants/Colors'
+
+const xyloSounds = {
+	one: require('./assets/note1.wav'),
+	two: require('./assets/note2.wav'),
+	three: require('./assets/note3.wav'),
+	four: require('./assets/note4.wav'),
+	five: require('./assets/note5.wav'),
+	six: require('./assets/note6.wav'),
+	seven: require('./assets/note7.wav')
+}
+
+export default function App() {
+	return (
+		<View style={styles.container}>
+			<View style={styles.buttonContainer}>
+				<TouchableOpacity
+					style={[styles.button, { backgroundColor: NoteOne }]}
+					onPress={() => this.handlePlaySound('one')}
+				>
+					<Text style={styles.buttonText}>Note 1</Text>
+				</TouchableOpacity>
+			</View>
+			<View style={styles.buttonContainer}>
+				<TouchableOpacity
+					style={[styles.button, { backgroundColor: NoteTwo }]}
+					onPress={() => this.handlePlaySound('two')}
+				>
+					<Text style={styles.buttonText}>Note 2</Text>
+				</TouchableOpacity>
+			</View>
+			<View style={styles.buttonContainer}>
+				<TouchableOpacity
+					style={[styles.button, { backgroundColor: NoteThree }]}
+					onPress={() => this.handlePlaySound('three')}
+				>
+					<Text style={styles.buttonText}>Note 3</Text>
+				</TouchableOpacity>
+			</View>
+			<View style={styles.buttonContainer}>
+				<TouchableOpacity
+					style={[styles.button, { backgroundColor: NoteFour }]}
+					onPress={() => this.handlePlaySound('four')}
+				>
+					<Text style={styles.buttonText}>Note 4</Text>
+				</TouchableOpacity>
+			</View>
+			<View style={styles.buttonContainer}>
+				<TouchableOpacity
+					style={[styles.button, { backgroundColor: NoteFive }]}
+					onPress={() => this.handlePlaySound('five')}
+				>
+					<Text style={styles.buttonText}>Note 5</Text>
+				</TouchableOpacity>
+			</View>
+			<View style={styles.buttonContainer}>
+				<TouchableOpacity
+					style={[styles.button, { backgroundColor: NoteSix }]}
+					onPress={() => this.handlePlaySound('six')}
+				>
+					<Text style={styles.buttonText}>Note 6</Text>
+				</TouchableOpacity>
+			</View>
+			<View style={styles.buttonContainer}>
+				<TouchableOpacity
+					style={[styles.button, { backgroundColor: NoteSeven }]}
+					onPress={() => this.handlePlaySound('seven')}
+				>
+					<Text style={styles.buttonText}>Note 7</Text>
+				</TouchableOpacity>
+			</View>
+		</View>
+		
+	)
+}
